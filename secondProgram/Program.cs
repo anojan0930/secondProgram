90947065c3253891411f0844ba7a1452f95dfd84
ut01520 <ut01520@UNICOMTIC34>
2024-06-10 15:58:57 +0530

    Add project files.

---
+++ secondProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        printHello();
+        GreetUser("Anojan");
+        MultibleMethod("Anojan","Anoj");
+    }
+    static void printHello()
+    {
+        Console.WriteLine("hello,world");
+
+    }
+    static void GreetUser(string name)
+    {
+        Console.WriteLine("hello "+name);
+    }
+    static void MultibleMethod(string name , string name1)
+    {
+        Console.WriteLine(name +  \n  + name1);
+    }
+}
